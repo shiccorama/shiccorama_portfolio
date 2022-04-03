29020232ad144d87dad84afc45235ec142ffae32
shiccorama <shiccorama@gmail.com>
2022-04-04 01:04:14 +0200

    adding Skills to home page and models to DB

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_31_035615) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_03_224314) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "skills", force: :cascade do |t|
+    t.string "title"
+    t.integer "percentile"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
