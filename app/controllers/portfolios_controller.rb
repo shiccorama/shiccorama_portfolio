cd464b52e7e84c7b7ccd318830fc6b5670ee505b
shiccorama <shiccorama@gmail.com>
2022-04-08 05:20:34 +0200

    integrated new and create methods into our app

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -9,6 +9,34 @@ class PortfoliosController < ApplicationController
 	end
 
 
+	def new
+		@portfolios = Portfolio.new
+	end
+
+	def edit
+	end
+
+	def create
+		@portfolios = Portfolio.new(portfolio_params)
+
+		respond_to do |format|
+			if @portfolios.save
+				format.html { redirect_to portfolios_path, notice: "Portfolio created successfully."}
+			else
+				format.html {render :new, status: :unprocessable_entity}
+			end
+		end
+
+	end
+
+
+	private
+
+	def portfolio_params
+		params.require(:portfolio).permit(:title, :subtitle, :body, :main_image, :thumb_image)
+	end
+
+
 
 
 
