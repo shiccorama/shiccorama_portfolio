3eb469c0c9c0ca303e5cf2d1dce9b41964e17a49
shiccorama <shiccorama@gmail.com>
2022-05-04 09:15:28 +0200

    updating portfolio features branch

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,2 +1,93 @@
 class PortfoliosController < ApplicationController
+
+	def index
+		@portfolios = Portfolio.all
+	end
+
+
+	def show
+		@portfolios = Portfolio.find(params[:id])
+	end
+
+
+	def new
+		@portfolios = Portfolio.new
+	end
+
+
+	def create
+		@portfolios = Portfolio.new(portfolio_params)
+
+		respond_to do |format|
+			if @portfolios.save
+				format.html { redirect_to portfolios_url, notice: "Portfolio created successfully."}
+			else
+				format.html {render :new, status: :unprocessable_entity}
+			end
+		end
+
+	end
+
+
+
+	def edit
+		@portfolios = Portfolio.find(params[:id])
+	end
+
+
+	def update
+		@portfolios = Portfolio.find(params[:id])
+
+		respond_to do |format|
+			if @portfolios.update(portfolio_params)
+				format.html { redirect_to portfolios_path, notice: "Portfolio updated successfully."}
+			else
+				format.html {render :new, status: :unprocessable_entity}
+			end
+		end
+	end
+
+	def destroy
+		@portfolios = Portfolio.find(params[:id])
+
+		@portfolios.destroy
+
+		respond_to do |format|
+			format.html { redirect_to portfolios_path, notice: "Blog was successfully destroyed." }
+		end
+	end
+
+
+	private
+
+	def portfolio_params
+		params.require(:portfolio).permit(:title, :subtitle, :body, :main_image, :thumb_image)
+	end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+10.times do |i|
+    Blog.create!(
+    title: "my title no. #{i}",
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
+end
+
+puts "10 blogs created"
+
+
+6.times do |s|
+    Skill.create!(
+        title: "my skill no. #{s}",
+        percentile: "#{s*12}"
+        )
+end
+
+puts "6 skills created"
+
+9.times do |p|
+    Portfolio.create!(
+        title: "my portfolio no. #{p}",
+        subtitle: "this is my subtitle no. #{p}",
+        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
+        main_image: "https://placekitten.com/500/400",
+        thumb_image:"https://placekitten.com/300/200"
+
+        )
+end
+
+puts "9 portfolios created"
